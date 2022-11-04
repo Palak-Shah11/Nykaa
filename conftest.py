fcecbf5dfa4f23b7c2206a891c1ba1ef2ab4932b
Palak-Shah11 <palakbshah1011@gmail.com>
2022-11-04 11:53:26 +0530

    final changed

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())
 
 
 @pytest.fixture()
